5304b80b9ae50565b8d66866bab63f2944eb0ce8
David Pinon <david.pinon.dse@uteq.edu.mx>
2018-09-07 14:29:08 -0500

    Section added by Ray 07.09.2018

--- Practica1444__Fucion1.c
+++ Practica1444__Fucion1.c
@@ -6,6 +6,14 @@
 *
 */
 
+/*Begin
+* Section added by Ray - 07.09.2018*/
+#include "stdio.h"
+#include "math.h"
+/*End
+* Section added by Ray - 07.09.2018*/
+
+
 
 int Practica1444__funcion1(Void)
 {
