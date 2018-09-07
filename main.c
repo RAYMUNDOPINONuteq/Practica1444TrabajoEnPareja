e0b62d6d7be3d0b462214cffdd60a221824fb54b
Marlen <marlen.gonzalez.dse@uteq.edu.mx>
2018-09-07 12:50:26 -0500

    Add main file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int function1();
+void procedure1();
+
+int main()
+{
+	int number = 0;
+	number = function1();
+	procedure1();
+	return 0;
+}
